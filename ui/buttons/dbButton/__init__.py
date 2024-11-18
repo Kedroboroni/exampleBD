94891b89a6311158c2bb01bbdcad7ee424387fa0
kedroboroni <svishoyrik@mail.ru>
2024-11-18 13:19:19 +0300

    описали кнопки, для sidebar

---
+++ ui/buttons/dbButton/__init__.py
@@ -0,0 +1,37 @@
+from PySide6.QtWidgets import QPushButton, QApplication, QWidget, QVBoxLayout
+from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import QSize
+import sys
+
+
+
+class dbButton(QPushButton):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.setFixedSize(50,50)
+
+        icon_path = r"ui\buttons\dbButton\db.png"
+        icon = QIcon(QPixmap(icon_path))
+        self.setIcon(icon)
+
+        self.setIconSize(QSize(41,41))
+        
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    with open(r"ui\SpyBot.qss", "r") as file:
+            style = file.read()
+            app.setStyleSheet(style)  
+    window = QWidget()
+    layout = QVBoxLayout()
+    btn1 = dbButton()
+    window.setLayout(layout)
+    layout.addWidget(btn1)
+    window.resize(300, 200)
+    window.show()
+    sys.exit(app.exec())
